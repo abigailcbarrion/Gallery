cec8c7b6d53aad4ed04e4a8c944abe26a690363c
abigailcbarrion <abigailcbarrion@iskolarngbayan.pup.edu.ph>
2025-11-26 18:46:30 +0800

    add directors section

--- data.js
+++ data.js
@@ -1,18 +1,58 @@
 var filmStrip = [
-  {
-    "src": "assets/images/herosmall.png",
-    "alt": "Image 1"
-  },
-  {
-    "src": "assets/images/herosmall.png",
-    "alt": "Image 2"
-  },
-  {
-    "src": "assets/images/herosmall.png",
-    "alt": "Image 3"
-  },
-  {
-    "src": "assets/images/herosmall.png",
-    "alt": "Image 4"
-  }
+    {
+        "src": "assets/images/herosmall.png",
+        "alt": "Image 1"
+    },
+    {
+        "src": "assets/images/herosmall.png",
+        "alt": "Image 2"
+    },
+    {
+        "src": "assets/images/herosmall.png",
+        "alt": "Image 3"
+    },
+    {
+        "src": "assets/images/herosmall.png",
+        "alt": "Image 4"
+    }
 ];
+
+var directors = [
+    {
+        "image": "assets/images/greta.jpg",
+        "name": "Greta Gerwig",
+        "description": "Greta Gerwig has become a real trailblazer for women in the film industry. After a stellar 2023, when her blockbuster, Barbie, painted the world pink, the director's stock has never been higher. She has turned in winner after winner since 2017's Lady Bird. She then followed the successful A24 movie with the acclaimed Little Women and then Barbie, which, at $1.4 billion worldwide, is the 14th highest-grossing movie of all time.",
+        "page": "Greta.html"
+    },
+    {
+        "image": "assets/images/wes.jpg",
+        "name": "Wes Anderson",
+        "description": "Only a few people can do comedy the way Wes Anderson does. Despite his first two films, Bottle Rocket and Rushmore coming out at the tail end of the 1990's, most of his work has been released in the 2000s and onward. The Royal Tenenbaums kicked off a run of films that also includes The Life Aquatic with Steve Zissou, The Darjeeling Limited, The Fantastic Mr. Fox, Moonrise Kingdom, The Grand Budapest Hotel, Isle of Dogs, etc.",
+        "page": "wes.html"
+    },
+    {
+        "image": "assets/images/quentin.jpg",
+        "name": "Quentin Tarantino",
+        "description": "Quentin Tarantino needs no introduction. The director's cinematic releases have industry-wide events and while he certainly redefined the 1990s with Reservoir Dogs and Pulp Fiction, the large portion of his cinematic outings have been in the 21st century. Kill Bill, Django Unchained, Once Upon a Time in Hollywood, and Inglourious Basterds to name a few.",
+        "page": "quentin.html"
+    },
+    {
+        "image": "assets/images/bong.jpg",
+        "name": "Bong Joon-Ho",
+        "description": "Bong Joon-ho is a South Korean filmmaker who is a defining filmmaker of the 21st century. He is one of the biggest voices in cinema that has reached a global audience. His latest film, Parasite, became a critical favorite and defied all expectations to win Best Foreign Language Film, Best Director, and Best Picture, making it the first time a non-American production won the top prize at the Academy Awards.",
+        "page": "bong.html"
+    },
+    {
+        "image": "assets/images/david.jpg",
+        "name": "David Fincher",
+        "description": "Most people would be afraid to know what's on David Fincher's mind. While some of the director's most famous movies might have come out in the 1990s, like Fight Club and Se7en, the 21st century has featured some of his most acclaimed The Social Network, The Girl with the Dragon Tattoo, Zodiac, Gone Girl, and most recently, The Killer.",
+        "page": "david.html"
+    },
+    {
+        "image": "assets/images/christopher.jpg",
+        "name": "Christopher Nolan",
+        "description": "Christopher Nolan is a name that has refined movies in the 21st century on a level that he is well-known and beloved by critics, film fans, and mainstream movie audiences alike. Nolan is known for his intricate, mind-bending narratives and stunning visuals, achieved mainly with practical effects. His latest film, Oppenheimer, speaks to his power as a filmmaker.",
+        "page": "christopher.html"
+    }
+];
+
