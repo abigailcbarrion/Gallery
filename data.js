78401066b09dbd530461b14afd65948e02396dfc
abigailcbarrion <abigailcbarrion@iskolarngbayan.pup.edu.ph>
2025-11-26 16:57:18 +0800

    added footer and filmstrip section

---
+++ data.js
@@ -0,0 +1,18 @@
+var filmStrip = [
+  {
+    "src": "assets/images/herosmall.png",
+    "alt": "Image 1"
+  },
+  {
+    "src": "assets/images/herosmall.png",
+    "alt": "Image 2"
+  },
+  {
+    "src": "assets/images/herosmall.png",
+    "alt": "Image 3"
+  },
+  {
+    "src": "assets/images/herosmall.png",
+    "alt": "Image 4"
+  }
+];
